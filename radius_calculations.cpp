fdd57bf48e6a00ab69f1f73172f0dd7cd19806d8
Ubuntu <ubuntu@ip-172-31-28-83.ec2.internal>
2019-09-16 22:33:00 +0000

    finished unit 2-01 cpp code

---
+++ radius_calculations.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2019 DJ Watson All Rights Reserved
+//
+// Created by DJ Watson
+// Created on September 2019
+
+
+#include <iostream>
+#include <cmath>
+
+
+int main() {
+    std::cout << "If a circle has a radius of 15mm" << std::endl;
+    std::cout << "AREA: " << (M_PI*pow(15, 2)) << "mm^2" << std::endl;
+    std::cout << "PERIMETER: " << (2*M_PI*15) << "mm" << std::endl;
+}
